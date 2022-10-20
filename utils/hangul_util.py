8f7ff9e0d38afb19dbaf4d7fe7347b766d7e6071
TakWolf <takwolf@foxmail.com>
2022-10-20 16:27:38 +0800

    完成工具类

---
+++ utils/hangul_util.py
@@ -0,0 +1,31 @@
+
+# 初声辅音（声母），共 19 个
+initial_consonants = ['ㄱ', 'ㄲ', 'ㄴ', 'ㄷ', 'ㄸ', 'ㄹ', 'ㅁ', 'ㅂ', 'ㅃ', 'ㅅ', 'ㅆ', 'ㅇ', 'ㅈ', 'ㅉ', 'ㅊ', 'ㅋ', 'ㅌ', 'ㅍ', 'ㅎ']
+
+# 中声元音（韵母），共 21 个
+vowels = ['ㅏ', 'ㅐ', 'ㅑ', 'ㅒ', 'ㅓ', 'ㅔ', 'ㅕ', 'ㅖ', 'ㅗ', 'ㅘ', 'ㅙ', 'ㅚ', 'ㅛ', 'ㅜ', 'ㅝ', 'ㅞ', 'ㅟ', 'ㅠ', 'ㅡ', 'ㅢ', 'ㅣ']
+
+# 终声辅音（韵尾），共 27 个
+final_consonants = ['ㄱ', 'ㄲ', 'ㄳ', 'ㄴ', 'ㄵ', 'ㄶ', 'ㄷ', 'ㄹ', 'ㄺ', 'ㄻ', 'ㄼ', 'ㄽ', 'ㄾ', 'ㄿ', 'ㅀ', 'ㅁ', 'ㅂ', 'ㅄ', 'ㅅ', 'ㅆ', 'ㅇ', 'ㅈ', 'ㅊ', 'ㅋ', 'ㅌ', 'ㅍ', 'ㅎ']
+
+# 音节总数
+syllables_count = len(initial_consonants) * len(vowels) * (len(final_consonants) + 1)
+assert syllables_count == 11172
+
+# 码域范围
+unicode_block_start = 0xAC00
+unicode_block_end = 0xD7AF
+
+
+def get_vowel_placement_mode(vowel):
+    """
+    获取元音在音节中的放置模式
+    """
+    if vowel in ['ㅏ', 'ㅐ', 'ㅑ', 'ㅒ', 'ㅓ', 'ㅔ', 'ㅕ', 'ㅖ', 'ㅣ']:
+        return 'vertical'
+    elif vowel in ['ㅗ', 'ㅛ', 'ㅜ', 'ㅟ', 'ㅠ', 'ㅡ']:
+        return 'horizontal'
+    elif vowel in ['ㅘ', 'ㅙ', 'ㅚ', 'ㅝ', 'ㅞ', 'ㅟ', 'ㅢ']:
+        return 'wrapping'
+    else:
+        raise Exception(f"'{vowel}' is not a vowel")
