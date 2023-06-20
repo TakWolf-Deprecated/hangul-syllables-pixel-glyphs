f3a0b7777a36916f83cfc5ca5f7ce3ffbd19426a
TakWolf <takwolf@foxmail.com>
2023-06-20 16:28:06 +0800

    调整格式化逻辑

--- services/glyph_service.py
+++ services/glyph_service.py
@@ -10,69 +10,43 @@ from utils import fs_util, hangul_util
 logger = logging.getLogger('glyph-service')
 
 
-def _load_glyph_data_from_png(file_path: str) -> tuple[list[list[int]], int, int]:
-    width, height, bitmap, _ = png.Reader(filename=file_path).read()
-    data = []
-    for bitmap_row in bitmap:
-        data_row = []
-        for x in range(0, width * 4, 4):
-            alpha = bitmap_row[x + 3]
-            if alpha > 127:
-                data_row.append(1)
-            else:
-                data_row.append(0)
-        data.append(data_row)
-    return data, width, height
-
-
-def _save_glyph_data_to_png(data: list[list[int]], file_path: str):
-    bitmap = []
-    for data_row in data:
-        bitmap_row = []
-        for x in data_row:
-            bitmap_row.append(0)
-            bitmap_row.append(0)
-            bitmap_row.append(0)
-            if x == 0:
-                bitmap_row.append(0)
-            else:
-                bitmap_row.append(255)
-        bitmap.append(bitmap_row)
-    png.from_array(bitmap, 'RGBA').save(file_path)
-
-
-def _walk_jamo_dirs() -> Iterator[tuple[str, hangul_util.JamoType, hangul_util.HeightMode | None, hangul_util.MedialPlacementMode | None]]:
+def _walk_jamo_dirs() -> Iterator[tuple[str, hangul_util.JamoType, hangul_util.HeightMode | None, hangul_util.MedialPlacementMode | None, str]]:
     for jamo_type in hangul_util.JamoType:
         if jamo_type == hangul_util.JamoType.INITIAL:
             for height_mode in hangul_util.HeightMode:
                 for placement_mode in hangul_util.MedialPlacementMode:
                     for jamo in hangul_util.INITIAL_JAMOS:
-                        jamo_file_dir_name = os.path.join(jamo_type, height_mode, placement_mode, jamo)
-                        yield jamo_file_dir_name, jamo_type, height_mode, placement_mode
+                        jamo_dir_name = os.path.join(jamo_type, height_mode, placement_mode, jamo)
+                        yield jamo, jamo_type, height_mode, placement_mode, jamo_dir_name
         elif jamo_type == hangul_util.JamoType.MEDIAL:
             for height_mode in hangul_util.HeightMode:
                 for jamo in hangul_util.MEDIAL_JAMOS:
                     placement_mode = hangul_util.MedialPlacementMode.get_by_jamo(jamo)
-                    jamo_file_dir_name = os.path.join(jamo_type, height_mode, placement_mode, jamo)
-                    yield jamo_file_dir_name, jamo_type, height_mode, placement_mode
+                    jamo_dir_name = os.path.join(jamo_type, height_mode, placement_mode, jamo)
+                    yield jamo, jamo_type, height_mode, placement_mode, jamo_dir_name
         elif jamo_type == hangul_util.JamoType.FINAL:
             height_mode = None
             placement_mode = None
             for jamo in hangul_util.FINAL_JAMOS:
-                jamo_file_dir_name = os.path.join(jamo_type, jamo)
-                yield jamo_file_dir_name, jamo_type, height_mode, placement_mode
+                jamo_dir_name = os.path.join(jamo_type, jamo)
+                yield jamo, jamo_type, height_mode, placement_mode, jamo_dir_name
 
 
 def format_jamo_files(font_size: int):
     root_dir = os.path.join(path_define.jamos_dir, str(font_size))
     tmp_dir = os.path.join(path_define.jamos_tmp_dir, str(font_size))
     fs_util.delete_dir(tmp_dir)
-    for jamo_file_dir_name, jamo_type, height_mode, placement_mode in _walk_jamo_dirs():
-        jamo_file_from_dir = os.path.join(root_dir, jamo_file_dir_name)
-        jamo_file_to_dir = os.path.join(tmp_dir, jamo_file_dir_name)
+    for jamo, jamo_type, height_mode, placement_mode, jamo_dir_name in _walk_jamo_dirs():
+        jamo_file_from_dir = os.path.join(root_dir, jamo_dir_name)
+        jamo_file_to_dir = os.path.join(tmp_dir, jamo_dir_name)
         fs_util.make_dirs(jamo_file_to_dir)
-
-        # TODO
+        for jamo_file_from_name in os.listdir(jamo_file_from_dir):
+            jamo_file_from_path = os.path.join(jamo_file_from_dir, jamo_file_from_name)
+            width, height, bitmap, _ = png.Reader(filename=jamo_file_from_path).read()
+            jamo_file_to_name = f'{jamo} {width}*{height}.png'
+            jamo_file_to_path = os.path.join(jamo_file_to_dir, jamo_file_to_name)
+            png.from_array(bitmap, 'RGBA').save(jamo_file_to_path)
+            logger.info(f"Format jamo file: '{jamo_file_to_path}'")
     fs_util.delete_dir(root_dir)
     os.rename(tmp_dir, root_dir)
 
